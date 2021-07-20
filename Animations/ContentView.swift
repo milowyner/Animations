84ba931bde39fa038915faccfcf4725b33981a06
Milo Wyner <milowyner@gmail.com>
2021-07-20 11:42:37 -0700

    Initial Commit

---
+++ Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Milo Wyner on 7/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
