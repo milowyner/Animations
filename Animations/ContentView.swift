fa22f0a19fe209467fc29fe9f738dc2e3f62a188
Milo Wyner <milowyner@gmail.com>
2021-07-20 12:28:37 -0700

    Pulsing circle animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount: CGFloat = 1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Me") {
+//            animationAmount += 1
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(animationAmount)
+                .opacity(Double(2 - animationAmount))
+                .animation(
+                    .easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+                )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
     }
 }
 
